e216f4c21ee5353271fd149e4f70d84a67686fac
Brave <august8821@naver.com>
2024-01-16 16:53:54 +0900

    ID 로그인

--- naver/script/login.js
+++ naver/script/login.js
@@ -6,10 +6,9 @@
 
 // 제목 3개 내용 3개 
 
-const login_title= document.querySelectorAll(".login_title h2 > a")
-
+const login_title = document.querySelectorAll(".login_title h2 > a")
+/* const login_title_id= document.querySelectorAll(".login_title h2 > a") */
 // 내용 변수 
-
 const id_login_container = document.querySelector(".login_container .id_login")
 const disposable_login_container = document.querySelector(".login_container .disposable_login")
 const qr_login_container = document.querySelector(".login_container .qr_login")
@@ -18,37 +17,52 @@ console.log(id_login_container)
 console.log(disposable_login_container)
 console.log(qr_login_container)
 
+
+
+
 //초기 일회용  qr내용 숨기기
-disposable_login_container.style.display= "none"
-qr_login_container.style.display="none"
+disposable_login_container.style.display = "none"
+qr_login_container.style.display = "none"
+
+// 초기값 아이디 로그인 활성화 시키기 active 클래스 활성화  *클릭 전 
+login_title[0].parentElement.classList.add("active")
+
+
+
 
 // 일회용 번호 클릭하면 로그인 내용 x 일회용 내용 o qr내용 x
 
-login_title[1].addEventListener("click", function(){
-
-    id_login_container.style.display= "none"
+login_title[1].addEventListener("click", function () {
+    id_login_container.style.display = "none"
     disposable_login_container.style.display = "block"
-    qr_login_container.style.display="none"
-    
+    qr_login_container.style.display = "none"
+    login_title[1].parentElement.classList.add("active");
+    login_title[0].parentElement.classList.remove("active");
+    login_title[2].parentElement.classList.remove("active");
 })
 
-// id 로그인을 클릭하면 로그인 내용 o 일회용 내용 x qr코드내용 x 
+// id 로그인을 클릭하면 로그인 내용 o 일회용 용 x qr코드내용 x 
 
-login_title[0].addEventListener("click", function(){
+login_title[0].addEventListener("click", function () {
 
-    id_login_container.style.display= "block"
+    id_login_container.style.display = "block"
     disposable_login_container.style.display = "none"
-    qr_login_container.style.display="none"
+    qr_login_container.style.display = "none"
+    login_title[0].parentElement.classList.add("active");
+    login_title[1].parentElement.classList.remove("active");
+    login_title[2].parentElement.classList.remove("active");
 
 })
 
 // qr코드 클릭하면 로그인내용 x, 일회용 내용 x, qr코드내용 o
 
-login_title[2].addEventListener("click",function(){
-    id_login_container.style.display= "none"
+login_title[2].addEventListener("click", function () {
+    id_login_container.style.display = "none"
     disposable_login_container.style.display = "none"
-    qr_login_container.style.display="block"
-
+    qr_login_container.style.display = "block"
+    login_title[2].parentElement.classList.add("active")
+    login_title[0].parentElement.classList.remove("active")
+    login_title[1].parentElement.classList.remove("active")
 })
 
 /* 탭 3개 누를 시 메뉴 나오는 것 까지 작업 0115 */
