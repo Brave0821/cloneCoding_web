0224ca1aca45a30ca1a2ff38a4e6612fa1331cdc
Brave <august8821@naver.com>
2024-01-22 18:16:08 +0900

    진행중

---
+++ scroll-animation2/script/scroll1.js
@@ -0,0 +1,22 @@
+const bg = document.querySelectorAll(".bg")
+// const link_a = document.querySelectorAll(".link a")
+const btn = document.querySelectorAll(".hom_btn #btn")
+
+btn.forEach((t,i)=>{
+    t.addEventListener("click",()=>{
+        window.scrollTo(0, bg[i].offsetTop)
+    })
+})
+
+// a태그로 한 경우 
+
+/* link_a.forEach((t,i)=>{
+    t.addEventListener("click",(e)=>{
+        e.preventDefault()
+        console.log(e)
+        window.scrollTo(0, bg[i].offsetTop)
+    })
+})
+ */
+/* 클릭하는 t가 a  */
+// 저장 관련해서 추가 
